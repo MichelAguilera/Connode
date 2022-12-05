54a82788cc224dc0ee2ba6402bd02c5544cdccd3
MichelAguilera <48520019+MichelAguilera@users.noreply.github.com>
2022-12-05 21:15:43 +0100

    init

---
+++ Connode.lua
@@ -0,0 +1,18 @@
+Connode = {}
+
+function Connode.defineLine(
+    node_number, separation, 
+    origin_x, origin_y, origin_z, 
+    angle_x, angle_y, angle_z, 
+    subject_angle_x, subject_angle_y, subject_angle_z
+)
+
+    
+
+end
+
+function Connode.drawNode()
+
+end
+
+return Connode
