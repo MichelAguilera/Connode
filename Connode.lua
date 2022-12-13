aa97c6fce9343b9d2ea4a7ae0b310cead3aaaed7
MichelAguilera <48520019+MichelAguilera@users.noreply.github.com>
2022-12-13 10:56:56 +0000

    V0.1.1

--- Connode.lua
+++ Connode.lua
@@ -8,15 +8,15 @@ function Connode.defineLine(
     subject_angle_x, subject_angle_y, subject_angle_z
 )
     local self = setmetatable({}, Connode)
-    self.separation = separation or 5
+    self.separation = separation
     
     self.origin_x = origin_x
     self.origin_y = origin_y
     self.origin_z = origin_z
     
-    self.angle_x = angle_x
-    self.angle_y = angle_y
-    self.angle_z = angle_z
+    self.angle_a = angle_z
+    self.angle_b = angle_x
+    self.angle_c = angle_y
 
     self.subject_angle_x = subject_angle_x or 0
     self.subject_angle_y = subject_angle_y or 0
@@ -27,8 +27,11 @@ end
 
 function Connode:drawNode(node_number)
 
-    local x = math.cos(self.angle_x) * self.separation * node_number
-    local y = math.sin(self.angle_x) * self.separation * node_number
+    local hypotenuse = self.separation * node_number
+
+    local y = hypotenuse * math.sin(self.angle_a)
+    local x = math.sqrt((hypotenuse)^2 - (y)^2)
+
     local z = 0
 
     local Node = Vector3.new(x, y, z)
